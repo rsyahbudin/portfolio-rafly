417a0e78e7e57ddf8afac06efb70774666e62b01
rsyahbudin <syahbudin45@gmail.com>
2024-10-31 10:37:16 +0700

    update logo dan cv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
-import logo from "../assets/raviKumarLogo.webp";
-
+import logo from "../assets/mrslogo.jpg";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
-          <img src={logo} className="mx-2" width={50} height={33} alt="logo" />
+          <img src={logo} className="mx-2 " width={50} height={33} alt="logo" />
         </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
@@ -27,7 +26,6 @@ const Navbar = () => {
         >
           <FaInstagram />
         </a>
-        
       </div>
     </nav>
   );
